151c9849e096835817565a9d76a7382cfeb45a8e
Jacinda Zhong <jacindaz@gmail.com>
2018-06-25 18:57:22 -0700

    Wk1,p2: connected components solution

--- week1/connected_components/connected_components.py
+++ week1/connected_components/connected_components.py
@@ -1,51 +1,50 @@
 #Uses python3
 import sys
+# import ipdb
 
-def visited_edges(all_adj_nodes):
+def number_of_components(adj):
     result = 0
-    visited_nodes = []
-    visited_edges = []
 
-    print(f"all_adj_nodes: {all_adj_nodes}")
+    visited = [0] * len(adj)
+    for i in range(len(adj)):
+        # print(f"\ni: {i}, visited[i]: {visited[i]}")
+        # print(f"visited: {visited}, result: {result}")
 
-    for index, adj_nodes in enumerate(all_adj_nodes):
-        print(f"----------------")
-        print(f"adj_nodes: {adj_nodes}, index: {index}")
-        print(f"visited_edges: {visited_edges}")
-        print(f"visited_nodes: {visited_nodes}")
+        # if visited[i] == 0, runs the clause
+        # if not visited[i]:
+        if visited[i] == 0:
+            # print(f"\ni: {i}, visited[i]: {visited[i]}")
+            # print(f"visited: {visited}, result: {result}")
 
-        current_index = index
-        visited_nodes.append(current_index)
+            explore(adj, i, visited)
+            result += 1
 
-        if len(adj_nodes) == 0:
-            visited_edges.append((current_index, None))
-        else:
-            for node in adj_nodes:
-                print(f"node: {node}")
-                sorted_visited_edge = sorted([current_index, node])
-                print(f"sorted_visited_edge: {sorted_visited_edge}")
+            # print(f"visited updated: {visited}")
+    return result
 
-                edge_we_are_visiting = tuple(sorted_visited_edge)
-                print(f"edge_we_are_visiting: {edge_we_are_visiting}")
 
-                if edge_we_are_visiting not in visited_edges:
-                    visited_edges.append(edge_we_are_visiting)
-                    print(f"new visited edge: {visited_edges}")
+def explore(adj, node_index, visited):
+    visited[node_index] = 1
 
-    return visited_edges
+    adjacent_nodes = adj[node_index]
+    for current_adj_node in adjacent_nodes:
+        current_node_visited = visited[current_adj_node]
 
-def number_of_components(all_adj_nodes):
-    adjacent_edges = visited_edges(all_adj_nodes)
-    # print(f"adjacent_edges: {adjacent_edges}")
-    connected_components = []
+        print("------------")
+        print(f"current_adj_node: {current_adj_node}")
+        print(f"visited: {visited}")
+        print(f"current_node_visited: {current_node_visited}")
+        if not current_node_visited:
+            explore(adj, current_adj_node, visited)
 
-    return connected_components
+
 
 # print(number_of_components([[1],[0,2],[1],[]])) # output: 2
 # print(number_of_components([[1],[0,2,3],[1,3],[2,1,4],[3], []]))
 
 # output: 2, connected components are: {4}, {1,2,3,5}
-print(number_of_components([[1],[0,2,4],[1],[],[1]]))
+# print(number_of_components([[1],[0,2,4],[1],[],[1]]))
+# print(explore([[1],[0,2,4],[1],[],[1]], 0, [0,0,0,0,0]))
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -53,14 +52,12 @@ if __name__ == '__main__':
     n, m = data[0:2]
     data = data[2:]
 
-    print(f"data before: {data}")
     edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
-    print(f"edges after: {edges}")
 
     adj = [[] for _ in range(n)]
     for (a, b) in edges:
         adj[a - 1].append(b - 1)
         adj[b - 1].append(a - 1)
 
-    print(f"adj: {adj}\n")
+    print(adj)
     print(number_of_components(adj))
